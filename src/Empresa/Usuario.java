56ab240f848b99ddd89451750d9a354d61f3dc3c
Lucas Alegre <lucasealegre123@gmail.com>
2022-02-16 15:07:45 -0300

    Version Usuario

--- src/Empresa/Usuario.java
+++ src/Empresa/Usuario.java
@@ -2,4 +2,24 @@ package Empresa;
 
 public class Usuario {
 
+	private String apellido;
+	private String nombre;
+
+	public Usuario(String apellido, String nombre) {
+		this.apellido = apellido;
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario( " + getApellido() + ", " + getNombre() + " )";
+	}
+	
 }
