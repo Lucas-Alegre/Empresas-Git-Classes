48aec688ee6feb449c0d33503ab16a739bdaf676
Lucas Alegre <lucasealegre123@gmail.com>
2022-02-16 15:32:34 -0300

    Revision en el main

--- src/Empresa/Main.java
+++ src/Empresa/Main.java
@@ -3,7 +3,13 @@ package Empresa;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Usuario persona = new Usuario("Alegre", "Lucas");
+		Region localidad = new Region("Concordia, Entre ríos, Argentina");
+		Empresa empresa = new Empresa(persona, localidad);
+		
+		System.out.println("-----------------Bienvenidos------------------");
+		System.out.println(empresa.toString());
+		System.out.println("-----------------Bienvenidos------------------");
 
 	}
 
