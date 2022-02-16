99a9cd89cb10cb0a0f8a0237097f6e0ba19f905b
Lucas Alegre <lucasealegre123@gmail.com>
2022-02-16 15:11:11 -0300

    Version Empresa

--- src/Empresa/Empresa.java
+++ src/Empresa/Empresa.java
@@ -1,5 +1,22 @@
 package Empresa;
 
 public class Empresa {
+	private Usuario usuario;
+	private Region region;
 
+	public Empresa(Usuario usuario, Region region) {
+		this.usuario = usuario;
+		this.region = region;
+	}
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public Region getRegion() {
+		return region;
+	}
+	@Override
+	public String toString() {
+		return "Empresa ( " + getUsuario() + ", " + getRegion() + " )";
+	}
 }
